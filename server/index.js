7d9c9acee2e5f24287f06c18323aa42863e73cce
Vadim Malckin <vadimproveril@gmail.com>
2018-10-25 23:26:32 +0300

    Mongoose added, Mongodb db connected

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const
   express = require('express'),
-  PORT = process.env.PORT || 5000,
-  app = express();
-
+  mongoose = require('mongoose'),
+  keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+const
+  app = express(),
+  PORT = process.env.PORT || 5000;
+
 require('./routes/authRoutes')(app);
+
 
 app.listen(PORT);
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,7 +1,10 @@
 const
   passport = require('passport'),
   GoogleStrategy = require('passport-google-oauth20').Strategy,
+  mongoose = require('mongoose'),
+  User = mongoose.model('users'),//User is a "Model Class" like Table in SQL
   keys = require('../config/keys');
+
 
 passport.use(
   new GoogleStrategy({
@@ -9,9 +12,6 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
   }, (accessToken, refreshToken, profile, done) => {
-    console.log(
-      `accessToken: ${accessToken}
-        refreshToken: ${refreshToken}`);
-    console.log("profile: "+ JSON.stringify(profile));
+    new User({googleId: profile.id, name: profile.name.givenName}).save();// Model Instance
   })
 );
